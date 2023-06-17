7b33c33bcc57c192b065843494242eaddacc630a
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-06-16 22:10:49 -0300

    init config

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Banner from '@/components/Banner'
+import Nav from '@/components/Nav'
+
+export default function Home() {
+  return (
+    <main className='h-[2000px]'>
+      <Nav />
+      <Banner />
+    </main>
+  )
+}
